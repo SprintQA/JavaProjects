3431d1f6116dd33873facf4b46ee687d610e8e84
S Erkam Alan <justsealan@gmail.com>
2020-01-26 23:19:19 +0000

    change

--- src/com/sprintqa/java/everyone/ClassSelahaddinAlan.java
+++ src/com/sprintqa/java/everyone/ClassSelahaddinAlan.java
@@ -3,15 +3,9 @@ package com.sprintqa.java.everyone;
 public class ClassSelahaddinAlan {
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
 	char [] myArr= {'s','a','p','b','r','c','i','d','n','e','t','g','q','h','a'};
 	for (int i=0; i<myArr.length; i+=2) {
 	System.out.println(myArr[i]);
-=======
-	char [] array= {'s','a','p','b','r','c','i','d','n','e','t','g','q','h','a'};
-	for (int i=0; i<array.length; i+=2) {
-	System.out.println(array[i]);
->>>>>>> d41b17f4ab2b00c622f7466fd891c2fd14891f1d
 	}
 	}
 }
