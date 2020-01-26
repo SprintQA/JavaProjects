f69e2a33cd1381e7f8fd854d6d32e83d71e0ec61
SprintQA <support@sprintqa.com>
2020-01-26 23:28:09 +0530

    Formatted output

--- src/com/sprintqa/java/class17/GitExample.java
+++ src/com/sprintqa/java/class17/GitExample.java
@@ -3,7 +3,7 @@ package com.sprintqa.java.class17;
 public class GitExample {
 
 	public static void main(String[] args) {
-		System.out.println("Git example");
+		System.out.println("Welcome to git example");
 
 	}
 
