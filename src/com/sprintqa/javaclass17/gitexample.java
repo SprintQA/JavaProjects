44c91d635512a49114d83b4495c0d89830064ab4
kcel80 <celkenan@gmail.com>
2020-01-26 12:12:55 -0800

    kenan cretead

--- src/com/sprintqa/javaclass17/gitexample.java
+++ src/com/sprintqa/javaclass17/gitexample.java
@@ -3,7 +3,7 @@ package com.sprintqa.javaclass17;
 public class gitexample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("you have to run daily");
 
 	}
 
