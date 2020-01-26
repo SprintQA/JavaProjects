fc822221b61ecb873ae667c842d6920cbf118f9b
S Erkam Alan <justsealan@gmail.com>
2020-01-26 22:23:45 +0000

    change in my code

--- src/com/sprintqa/java/everyone/ClassSelahaddinAlan.java
+++ src/com/sprintqa/java/everyone/ClassSelahaddinAlan.java
@@ -1,5 +1,11 @@
 package com.sprintqa.java.everyone;
 
 public class ClassSelahaddinAlan {
-
+	public static void main(String[] args) 
+	{
+	char [] array= {'s','a','p','b','r','c','i','d','n','e','t','g','q','h','a'};
+	for (int i=0; i<array.length; i+=2) {
+	System.out.println(array[i]);
+	}
+	}
 }
