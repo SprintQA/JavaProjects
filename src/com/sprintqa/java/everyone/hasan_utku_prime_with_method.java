3525bd9e1d28bb3dca7a458f56f4bf1c3a4388c0
hhutku <hhutku@users.noreply.github.com>
2020-01-28 23:05:19 -0800

    prime with method

---
+++ src/com/sprintqa/java/everyone/hasan_utku_prime_with_method.java
@@ -0,0 +1,47 @@
+package com.sprintqa.java.everyone;
+
+import java.util.Scanner;
+public class hasan_utku_prime_with_method {
+
+		public static int  prime() {
+			
+		
+			System.out.println(" enter a number : ");
+			Scanner sc=new Scanner(System.in);
+			int num1=sc.nextInt();
+			
+			boolean flag = false;
+
+			for (int i = 2; i <= num1 - 1; i++) {
+				
+				if(num1==2 || num1==1) {
+					break;
+				}
+				
+				if (num1 % i == 0) {
+					System.out.println("Number is not prime");
+					flag = true;
+					break;
+				}
+			}
+
+			if (flag == false) {
+				System.out.println("Number is prime number");
+			}
+
+			return num1;
+		}
+		
+					public static void main(String[] args) {
+
+				prime();
+				
+				
+				
+				
+				
+
+		
+	}
+
+}
