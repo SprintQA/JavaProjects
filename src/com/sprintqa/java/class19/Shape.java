f7127b3c8468cfce15fd3849f351b9364ba3cadb
mtafseerhaider <mtafseer.haider@gmail.com>
2020-01-30 07:17:49 +0500

    class 19 - OOP

---
+++ src/com/sprintqa/java/class19/Shape.java
@@ -0,0 +1,15 @@
+package com.sprintqa.java.class19;
+
+public class Shape
+{
+	// Methods
+	public void rotate ()
+	{
+		System.out.println ("rotate");
+	}
+
+	public void playSound ()
+	{
+		System.out.println ("play .mp3 sound");
+	}
+}
---
+++ src/com/sprintqa/java/class19/Square.java
@@ -0,0 +1,10 @@
+package com.sprintqa.java.class19;
+
+public class Square extends Shape
+{
+	// No Attributes
+	
+	
+	
+	
+}
