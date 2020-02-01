1e642794b0ac7858875cc56c8e9311d2604d2b33
SprintQA <support@sprintqa.com>
2020-02-02 02:20:12 +0530

    Class 22

--- src/com/sprintqa/java/class18/HomeWork.java
+++ src/com/sprintqa/java/class18/HomeWork.java
@@ -3,4 +3,8 @@ package com.sprintqa.java.class18;
 public class HomeWork {
 	// Design a function called isPrime(number) which will return true if number is prime and false if not prime
 	// Design a function called sumOfDigit(number) which will return sum of digit of given number
+	
+	public static void main(String[] args) {
+		
+	}
 }
