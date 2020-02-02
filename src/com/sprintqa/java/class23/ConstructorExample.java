f9c9e40d6f4b697ffb2bb8f0eac871486c533407
SprintQA <support@sprintqa.com>
2020-02-03 01:42:23 +0530

    Class 23

--- src/com/sprintqa/java/class23/ConstructorExample.java
+++ src/com/sprintqa/java/class23/ConstructorExample.java
@@ -1,16 +1,21 @@
 package com.sprintqa.java.class23;
 
 class HSBCAccount {
+	
+	// Program - Take pull from the github and write a program to count number of account that we opened from class
+//				 HSBCAccount or number of object we have created from class HSBCAccount
 
 	int balance;
 	int accountNumber;
 	String accountName;
+	static int count=0;
 
 	// Default constructor
 	public HSBCAccount() {
 		balance = 1000;
 		accountName = "xyz";
 		accountNumber = 1;
+		count++;
 	}
 
 	public void withdraw(int amount) {
@@ -26,6 +31,12 @@ public class ConstructorExample {
 		HSBCAccount account3 = new HSBCAccount();
 		HSBCAccount account4 = new HSBCAccount();
 		
+		HSBCAccount account11 = new HSBCAccount();
+		HSBCAccount account22 = new HSBCAccount();
+		HSBCAccount account33 = new HSBCAccount();
+		HSBCAccount account44 = new HSBCAccount();
+		
+		System.out.println(account4.count);
 		
 		
 	}
---
+++ src/com/sprintqa/java/class23/ConstructorExample2.java
@@ -0,0 +1,9 @@
+package com.sprintqa.java.class23;
+
+
+
+// Program - Write a program to use para constructor and default constructor 
+
+public class ConstructorExample2 {
+
+}
