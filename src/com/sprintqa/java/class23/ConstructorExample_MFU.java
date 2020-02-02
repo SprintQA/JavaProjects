be444ce012d8be8b070d5e5ab230381405294f7e
mfuyar <mfuyar@gmail.com>
2020-02-02 13:48:09 -0500

    NewFeb

--- src/com/sprintqa/java/class23/ConstructorExample_MFU.java
+++ src/com/sprintqa/java/class23/ConstructorExample_MFU.java
@@ -5,7 +5,7 @@ class HSBCAccount {
 	int balance;
 	int accountNumber;
 	String accountName;
-
+static int displayaccount;
 
 	// Default constructor
 	public HSBCAccount() {
@@ -21,18 +21,18 @@ class HSBCAccount {
 
 }
 
-public class ConstructorExample {
+public class ConstructorExample_MFU {
 	public static void main(String[] args) {
 		HSBCAccount account1 = new HSBCAccount();
-		
+		account1.displayaccount++;
 		HSBCAccount account2 = new HSBCAccount();
-		
+		account2.displayaccount++;
 		HSBCAccount account3 = new HSBCAccount();
-		
+		account3.displayaccount++;
 		HSBCAccount account4 = new HSBCAccount();
+		account4.displayaccount++;
 		
-		
-		
+		System.out.println(account4.displayaccount);
 		
 	}
 }
