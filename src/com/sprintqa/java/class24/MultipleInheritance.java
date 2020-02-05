9bcd9742d371a5a088ce52ee6dede6b9776a43f5
SprintQA <support@sprintqa.com>
2020-02-05 07:37:29 +0530

    Class 24 programs

---
+++ src/com/sprintqa/java/class24/MultipleInheritance.java
@@ -0,0 +1,29 @@
+package com.sprintqa.java.class24;
+
+
+class A{
+	public void displayLine() {
+		System.out.println("***************");
+	}
+}
+
+class B {
+	public void displayLine() {
+		System.out.println("###############");
+	}
+}
+
+class C extends A, B{
+	
+}
+
+
+public class MultipleInheritance {
+
+	public static void main(String[] args) {
+		C c1 = new C();
+		c1.displayLine();
+
+	}
+
+}
